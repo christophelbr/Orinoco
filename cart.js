c675f99c340ec8c7d3075cc96ea782cceb7300a1
christophelbr <c.lebren@icloud.com>
2021-01-10 19:06:15 +0100

    10122021

--- cart.js
+++ cart.js
@@ -8,10 +8,7 @@ class ShoppingCart {
             this.cart = []
         }
         this.countCart();
-        // this.clearCart();
-        /*         this.countCartLess();
-         */ //this.countCartMore(); 
-
+        this.clearCart();
     }
 
     loadCart() {
@@ -21,37 +18,22 @@ class ShoppingCart {
     saveCart() {
         localStorage.setItem('shoppingCart', JSON.stringify(this.cart));
     }
+
     countCart() {
         let count = 0;
+        let totalPrice = 0;
+
         for (const item of this.cart) {
             count += item.quantity;
+            totalPrice += item.price * item.quantity;
         }
         document.getElementById('total-count').innerHTML = count;
+        if (document.URL.indexOf("panier.html") >= 0) {
+            document.getElementById('totalPrice').innerHTML = totalPrice;
+        }
+        console.log(count);
+        console.log(totalPrice);
     }
-    /* countCartLess() {
-           for (let item of this.cart) {
-               if (item.name === item.name) {
-                   item.quantity--;
-                   if (item.quantity === 0) {
-                       this.cart.splice(item, 1);
-                   }
-               }
-           }  */
-    /* 
-            let countLess = itemLess.quantity;
-
-                for (let itemLess of this.cart) {
-                    countLess -= itemLess.quantity;
-                }
-            document.getElementsByClassName('less').innerHTML = countLess; */
-    // } 
-    /*     countCartMore() {
-            let countMore = item.quantity;
-            for (const item of this.cart) {
-                countMore += item.quantity;
-            }
-            document.getElementsByClassName('more').innerHTML = countMore;
-        }  */
 
     clearCart() {
         let btn = document.getElementById('clear');
@@ -60,9 +42,9 @@ class ShoppingCart {
             localStorage.clear();
             this.countCart();
             console.log(this.cart);
-
         })
     }
+
     addToCart(ourson) {
         let itemCart = new ItemCart(ourson.name, ourson.price, ourson._id, 1);
         const itemFound = this.cart.find(e => e.productId === ourson._id);
@@ -79,230 +61,51 @@ class ShoppingCart {
         console.log(this.cart);
         this.countCart();
     }
+
     displayCart() {
         let cartArray = this.cart;
-        let listePanier = document.getElementById('listePanier');
+        let listePanier = document.querySelector('#listePanier tbody');
         for (let item of cartArray) {
-            this.tr = document.createElement('tr');
-            this.tdName = document.createElement('td');
-            this.tdId = document.createElement('td');
-            this.tdPrice = document.createElement('td');
-            this.tdQuantity = document.createElement('td');
-            this.less = document.createElement('button');
-            this.less.textContent = '-'
-            this.more = document.createElement('button');
-            this.more.textContent = '+'
-            listePanier.appendChild(this.tr);
-            this.tr.appendChild(this.tdName);
-            this.tdName.textContent = item.name;
-            this.tr.appendChild(this.tdId);
-            this.tdId.textContent = item.productId;
-            this.tr.appendChild(this.tdPrice);
-            this.tdPrice.textContent = item.price;
-            this.tr.appendChild(this.tdQuantity);
-            this.tdQuantity.setAttribute('id', 'qantity'+ item.name);
-            this.tdQuantity.textContent = item.quantity;
-            this.tr.appendChild(this.less);
-            this.less.setAttribute('id', '-' + item.name);
-            this.less.addEventListener('click', () => {
+            const tr = document.createElement('tr');
+            const tdName = document.createElement('td');
+            const tdPrice = document.createElement('td');
+            const tdQuantity = document.createElement('td');
+            const less = document.createElement('button');
+            less.textContent = '-';
+            const more = document.createElement('button');
+            more.textContent = '+'
+            listePanier.appendChild(tr);
+            tr.appendChild(tdName);
+            tdName.textContent = item.name;
+            tr.appendChild(tdPrice);
+            tdPrice.textContent = item.price * item.quantity;
+            tr.appendChild(tdQuantity);
+            tdQuantity.setAttribute('id', 'qantity' + item.name);
+            tdQuantity.textContent = item.quantity;
+            tr.appendChild(less);
+            less.setAttribute('id', '-' + item.name);
+            less.addEventListener('click', () => {
                 item.quantity--;
                 localStorage.setItem("shoppingCart", JSON.stringify(this.cart));
                 if (item.quantity === 0) {
-                    this.cart.splice(item, 1);}
+                    this.cart.splice(item, 1);
+                }
                 this.countCart();
-                this.tdQuantity.textContent = item.quantity;
+                tdQuantity.textContent = item.quantity;
+                tdPrice.textContent = item.price * item.quantity;
             })
-            this.tr.appendChild(this.more);
-            this.more.classList.add('more');
-            this.more.addEventListener('click', () => {
+            tr.appendChild(more);
+            more.classList.add('more');
+            more.addEventListener('click', () => {
                 item.quantity++;
                 localStorage.setItem("shoppingCart", JSON.stringify(this.cart));
                 this.countCart();
-                this.tdQuantity.textContent = item.quantity;
+                tdQuantity.textContent = item.quantity;
+                tdPrice.textContent = item.price * item.quantity;
             })
-
-            console.log(item.name, item.productId, item.price, item.quantity);
+            console.log(item.name, item.price, item.quantity);
         }
 
     }
 
 }
-
-//let item = new ItemCart([0,1,2]);
-/* this.cart.push(item);
-saveCart(); */
-
-/*
-
-
-}
-
-
-    // =============================
-    // Méthodes et proproétés
-    // =============================
-    let obj = {};
-
-// Add to cart
-
-// Set count from item
-obj.setCountForItem = function (name, count) {
-    for (let i in cart) {
-        if (cart[i].name === name) {
-            cart[i].count = count;
-            break;
-        }
-    }
-};
-// Retirer objet du panier
-obj.removeItemFromCart = function (name) {
-    for (let item in cart) {
-        if (cart[item].name === name) {
-            cart[item].count--;
-            if (cart[item].count === 0) {
-                cart.splice(item, 1);
-            }
-            break;
-        }
-    }
-    saveCart();
-}
-
-// Enlever les articles du panier
-obj.removeItemFromCartAll = function (name) {
-    for (let item in cart) {
-        if (cart[item].name === name) {
-            cart.splice(item, 1);
-            break;
-        }
-    }
-    saveCart();
-}
-
-// vider le panier
-obj.clearCart = function () {
-    cart = [];
-    saveCart();
-}
-
-// Contage panier
-obj.totalCount = function () {
-    let totalCount = 0;
-    for (let item in cart) {
-        totalCount += cart[item].count;
-    }
-    return totalCount;
-}
-
-// Total panier
-obj.totalCart = function () {
-    let totalCart = 0;
-    for (let item in cart) {
-        totalCart += cart[item].price * cart[item].count;
-    }
-    return Number(totalCart.toFixed(2));
-}
-
-// List panier
-obj.listCart = function () {
-    let cartCopy = [];
-    for (i in cart) {
-        item = cart[i];
-        itemCopy = {};
-        for (p in item) {
-            itemCopy[p] = item[p];
-
-        }
-        itemCopy.total = Number(item.price * item.count).toFixed(2);
-        cartCopy.push(itemCopy)
-    }
-    return cartCopy;
-}
-
-// cart : Array
-// Item : Object/Class
-// addItemToCart : Function
-// removeItemFromCart : Function
-// removeItemFromCartAll : Function
-// clearCart : Function
-// countCart : Function
-// totalCart : Function
-// listCart : Function
-// saveCart : Function
-// loadCart : Function
-return obj;
-}) ();
-
-
-// *****************************************
-// Evenements
-// *****************************************
-// Add item
-$('.add-to-cart').click(function (event) {
-    event.preventDefault();
-    let name = $(this).data('name');
-    let price = Number($(this).data('price'));
-    shoppingCart.addItemToCart(name, price, 1);
-    displayCart();
-});
-
-// Clear items
-$('.clear-cart').click(function () {
-    shoppingCart.clearCart();
-    displayCart();
-});
-
-
-function displayCart() {
-    let cartArray = shoppingCart.listCart();
-    let output = "";
-    for (let i in cartArray) {
-        output += "<tr>"
-            + "<td>" + item.name + "</td>"
-            + "<td>(" + item.price + ")</td>"
-            + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + item.name + ">-</button>"
-            + "<input type='number' class='item-count form-control' data-name='" + item.name + "' value='" + item.count + "'>"
-            + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + item.name + ">+</button></div></td>"
-            + "<td><button class='delete-item btn btn-danger' data-name=" + item.name + ">X</button></td>"
-            + " = "
-            + "<td>" + item.total + "</td>"
-            + "</tr>";
-    }
-    $('.show-cart').html(output);
-    $('.total-cart').html(shoppingCart.totalCart());
-    $('.total-count').html(shoppingCart.totalCount());
-}
-
-// Delete item button
-
-$('.show-cart').on("click", ".delete-item", function (event) {
-    let name = $(this).data('name')
-    shoppingCart.removeItemFromCartAll(name);
-    displayCart();
-})
-
-
-// -1
-$('.show-cart').on("click", ".minus-item", function (event) {
-    let name = $(this).data('name')
-    shoppingCart.removeItemFromCart(name);
-    displayCart();
-})
-// +1
-$('.show-cart').on("click", ".plus-item", function (event) {
-    let name = $(this).data('name')
-    shoppingCart.addItemToCart(name);
-    displayCart();
-})
-
-// Item count input
-$('.show-cart').on("change", ".item-count", function (event) {
-    let name = $(this).data('name');
-    let count = Number($(this).val());
-    shoppingCart.setCountForItem(name, count);
-    displayCart();
-});
-
-displayCart();
-
- */
