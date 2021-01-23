f35b9c3fc7d59f1885bb7af97472a6575508e05f
christophelbr <c.lebren@icloud.com>
2021-01-23 12:54:11 +0100

    rm ORINOCO

--- ORINOCO/itemCart.js
+++
@@ -1,9 +0,0 @@
-class ItemCart {
-    constructor(name, price, productId, quantity) {
-        this.name = name;
-        this.price = price;
-        this.productId = productId;
-        this.quantity = quantity;
-    }
-
-}
